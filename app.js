396bef50b58994858635096bae06116b416ceb31
Denito37 <69406178+Denito37@users.noreply.github.com>
2022-11-19 18:56:29 -0500

    chart styling

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const myChart = document.getElementById('barChart').getContext('2d');
 let xValues = ['mon', 'tues', 'wed', 'thur', 'fri', 'sat', 'sun'];
 let yValues= ['17.45', '34.91', '52.36', '31.07', '23.39', '43.28', '25.48'];
 let barColors = "hsl(10 79% 65%)";
+const d = new Date();
+let day = d.getDay();
+
+console.log(day);
 
 const drawChart = new Chart(myChart, {
     type:'bar',
@@ -14,6 +18,10 @@ const drawChart = new Chart(myChart, {
         }]
     },
     options: {
-        legend: {display: false}
+        legend: {display: false},
+        scales: {
+            yAxes:[{gridLines:{display:false}}],
+            xAxes:[{gridLines:{display:false}}],
+        }
     }
 });
